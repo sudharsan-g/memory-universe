ad2ee7018749610f6b6429102ba2ee3b19796a9e
sudharsan-g <sudharsanganesan004@gmail.com>
2026-05-29 22:16:27 +0530

    changes

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="vite/client" />
 
 declare module 'canvas-confetti'
+declare module "*.mp3";
+declare module "*.mp4";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
 
 interface Window {
   webkitAudioContext?: typeof AudioContext
